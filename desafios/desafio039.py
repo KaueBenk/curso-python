9b59654a9679c4eb19c03c35bb3426a3a06fb9ab
Kauê Benk <kauebenk@gmail.com>
2021-02-18 15:06:48 -0300

    Desafio 39

---
+++ desafios/desafio039.py
@@ -0,0 +1,14 @@
+from datetime import date
+
+print('='*10, 'Desafio 039', '='*10, '\nIdade de Alistamento\n')
+
+ano = int(input('Informe o seu ano de nascimento: '))
+ano_atual = date.today().year
+idade = ano_atual - ano
+
+if idade < 18:
+    print(f'Em {18-idade} anos vai se alistar ao serviço militar.')
+elif idade == 18:
+    print('Já é a hora de se alistar!')
+else:
+    print(f'Já se passaram {idade-18} anos do seu tempo de alistamento.')
